0473f1ce8098af0987468323ac652ee958303d96
Milan Karman <milankarman@gmail.com>
2020-11-19 17:07:50 +0100

    Made header use header element instead of h1 class

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -23,11 +23,11 @@ const Layout = ({ children, title, headerText }: Props): JSX.Element => (
     </Row>
     <Row>
       <Col>
-        <h1 className="text-center align-middle mt-2 mb-0 color-yellow d-none d-md-block">{headerText}</h1>
-        <h1 className="text-center align-middle mt-2 mb-0 color-yellow d-none d-sm-block d-md-none h1-medium">
+        <header className="text-center align-middle mt-2 mb-0 color-yellow d-none d-md-block">{headerText}</header>
+        <header className="text-center align-middle mt-2 mb-0 color-yellow d-none d-sm-block d-md-none header-medium">
           {headerText}
-        </h1>
-        <h1 className="text-center align-middle mt-2 mb-0 color-yellow d-block d-sm-none h1-small">{headerText}</h1>
+        </header>
+        <header className="text-center align-middle mt-2 mb-0 color-yellow d-block d-sm-none header-small">{headerText}</header>
       </Col>
     </Row>
     <Row className="p-3 rounded bg-dark">

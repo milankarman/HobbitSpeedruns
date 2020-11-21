7bf44edf8e4bcf994d368d8a5720fd32dabd5a48
Milan Karman <milankarman@gmail.com>
2020-11-21 17:44:51 +0100

    Updated homepage text

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,13 +20,13 @@ const IndexPage = ({ gamecubeRuns, pcRuns }: Props): JSX.Element => (
         <h2>Welcome to HobbitSpeedruns</h2>
         <p>
           This website is home to information and resources about speedrunning the game &quot;The Hobbit&quot;. Which
-          was released on PC, GameCube, PS2 and XBox in 2004 by Sierra Entertainment. Compiled by its speedrunning
-          community.
+          was released on PC, GameCube, PS2 and XBox in 2004 by Sierra Entertainment.
         </p>
         <p>
           On here you will find guides for the speedrunning routes of the different platforms the game is on as well as
           links to community resources. This website was made with the hope of providing an accessible and central place
           one can come to find anything they might want to know when they are interesting in The Hobbit speedrunning.
+          Though this site does feature a leaderboard, for more complete leaderboards one should go to <a href="https://www.speedrun.com/hobbit"> speedrun.com. </a>
         </p>
         <p>
           We have a relatively small but welcoming community so feel free to join us on

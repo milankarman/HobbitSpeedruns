c38d148473c44b6593ab412f4db34ad960839ede
Milan Karman <milankarman@gmail.com>
2022-11-26 19:02:28 +0100

    Added boof guide, framework update, fixed hydration error on reactplayer

---
+++ data/guides/any-percent-nmg-pc-boof/guide.md
@@ -0,0 +1,30 @@
+---
+header: Any% NMG (PC)
+order: 0
+---
+
+## Any% No Major Glitches (PC)
+
+This guide covers setup as well as the full route for The Hobbit PC NMG speedruns. Packaged in one concise and complete video.
+
+<iframe width="560" height="315" src="https://www.youtube.com/watch?v=zAjNh4RA_44" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+0:00 - Introduction
+0:20 - Installation
+1:10 - InGame Settings
+2:55 - Basic Movement
+6:30 - HST Settings
+7:40 - L1 Dream World
+9:16 - L2 Unexpected Party
+10:45 - L3 Roast Mutton
+11:32 - L4 Troll Hole
+12:42 - L5 OverHill and UnderHill
+25:00 - L6 Riddles in The Dark
+32:43 - L7 Flies and Spiders
+39:10 - L8 Barrels Out of Bound
+41:55 - L9 A Warm Welcome
+44:01 - L10 Inside Information
+47:25 - L11 The Gathering of The Clouds
+49:30 - L12 The Clouds Burst
+58:10 - LiveSplit Setup
+59:30 - Obs Setup
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import { GetStaticProps } from 'next';
 import { Row, Col } from 'react-bootstrap';
 import { requestRuns } from '../lib/runs';
 import { ParsedRun } from '../interfaces/leaderboard';
-import ReactPlayer from 'react-player';
+import dynamic from 'next/dynamic';
+
+const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
 import Layout from '../components/Layout';
 import LeaderboardTable from '../components/LeaderboardTable';
@@ -12,44 +14,47 @@ type Props = {
   pcRuns: ParsedRun[];
 };
 
-const IndexPage = ({ gamecubeRuns, pcRuns }: Props): JSX.Element => (
-  <Layout title="Home | HobbitSpeedruns" headerText="hobbitspeedruns">
-    <Row>
-      <Col className="mb-3">
-        <h2>Welcome to HobbitSpeedruns</h2>
-        <p>
-          This website is home to information and resources about speedrunning the game &quot;The Hobbit&quot;. Which
-          was released on PC, GameCube, PS2 and XBox in 2003 by Sierra Entertainment.
-        </p>
-        <p>
-          On here you will find guides for the speedrunning routes of the different platforms the game is on as well as
-          links to community resources. This website was made with the hope of providing an accessible and central place
-          one can come to find anything they might want to know when they are interesting in The Hobbit speedrunning.
-          Though this site does feature a leaderboard, for more complete leaderboards one should go to <a href="https://www.speedrun.com/hobbit"> speedrun.com. </a>
-        </p>
-        <p>
-          We have a relatively small but welcoming community so feel free to join us on
-          <a href="https://discord.gg/uHRANWgUYZ"> discord </a>
-          and ask us anything about the game or just come and chat.
-        </p>
-        <p>Below is a long form documentary showcasing the game's rich speedrunning history.</p>
-        <ReactPlayer controls={true} width="100%" url="https://www.youtube.com/watch?v=WJndaDpohSY" />
-      </Col>
-      <Col xl={4} lg={12}>
-        <Row className="mb-3">
-          <Col xl={12} md={6} className="mb-3">
-            <h5>Top GameCube Times</h5>
-            <LeaderboardTable runs={gamecubeRuns} compact={true} top={5} />
-          </Col>
-          <Col xl={12} md={6}>
-            <h5>Top PC Times</h5>
-            <LeaderboardTable runs={pcRuns} compact={true} top={5} />
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  </Layout>
-);
+const IndexPage = ({ gamecubeRuns, pcRuns }: Props): JSX.Element => {
+  return (
+    <Layout title="Home | HobbitSpeedruns" headerText="hobbitspeedruns">
+      <Row>
+        <Col className="mb-3">
+          <h2>Welcome to HobbitSpeedruns</h2>
+          <p>
+            This website is home to information and resources about speedrunning the game &quot;The Hobbit&quot;. Which
+            was released on PC, GameCube, PS2 and XBox in 2003 by Sierra Entertainment.
+          </p>
+          <p>
+            On here you will find guides for the speedrunning routes of the different platforms the game is on as well
+            as links to community resources. This website was made with the hope of providing an accessible and central
+            place one can come to find anything they might want to know when they are interesting in The Hobbit
+            speedrunning. Though this site does feature a leaderboard, for more complete leaderboards one should go to{' '}
+            <a href="https://www.speedrun.com/hobbit"> speedrun.com. </a>
+          </p>
+          <p>
+            We have a relatively small but welcoming community so feel free to join us on
+            <a href="https://discord.gg/uHRANWgUYZ"> discord </a>
+            and ask us anything about the game or just come and chat.
+          </p>
+          <p>Below is a long form documentary showcasing the game`&apos`s rich speedrunning history.</p>
+          <ReactPlayer controls={true} width="100%" url="https://www.youtube.com/watch?v=WJndaDpohSY" />
+        </Col>
+        <Col xl={4} lg={12}>
+          <Row className="mb-3">
+            <Col xl={12} md={6} className="mb-3">
+              <h5>Top GameCube Times</h5>
+              <LeaderboardTable runs={gamecubeRuns} compact={true} top={5} />
+            </Col>
+            <Col xl={12} md={6}>
+              <h5>Top PC Times</h5>
+              <LeaderboardTable runs={pcRuns} compact={true} top={5} />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Layout>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const [gamecubeRuns, pcRuns] = await requestRuns();
